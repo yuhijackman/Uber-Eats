a01b332ed9d7fc3ed6cc34bf7f8506a18577f3d2
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-03-25 12:10:52 +0900

    Display user pages

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,9 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   # thumb バージョン(width 400px x height 200px)
-  version :thumb do
-    process resize_to_fit: [400, 200]
-  end
 
   # 許可する画像の拡張子
   def extension_white_list
