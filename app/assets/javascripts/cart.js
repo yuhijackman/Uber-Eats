3338136c482fa08217d3f5113b68c7a2ed19f636
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-11 17:17:19 +0900

    Modify Cart Js

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,4 +1,29 @@
 $(function() {
+  function buildHTML(cart_detail) {
+    var name = cart_detail.name;
+    var price = cart_detail.price;
+    var menu_id = cart_detail.id;
+    var item = $( '<div class= "cart__list__content__left__col col-md-2">'
+               + '</div>'
+               + '<div class= "cart__list__content__center__col col-md-7">'
+               + '<class = "modal-body">'
+               + name
+               + '</div>'
+               + '</div>'
+               + '<div class= "cart__list__content__right__col col-md-3">'
+               + '<div class= "modal-price">'
+               + "¥" + price
+               + '</div>'
+               + '</div>');
+    $('.cart__list__content').append(item);
+  }
+
+  function showAlert() {
+    var notification = $( '<div class= "empty__notice col-md-12">'
+                       + "カートが空です。"
+                       + '</div>');
+    $('.cart__list__content').append(notification);
+  }
 
   $('.cart__details').on('click',function() {
     $.ajax({
@@ -8,11 +33,13 @@ $(function() {
     })
 
     .done(function(data) {
+      if(data == null){
+        $('.cart__list__footer').remove();
+        showAlert();
+      }
       $.each(data, function(i, menu_info) {
-        var name = menu_info.name
-        var price = menu_info.price
-        $('.modal-body').append(name);
-        $('.modal-body').append(price);
+        var cart_detail = menu_info
+        buildHTML(cart_detail)
       });
     })
 
@@ -22,5 +49,3 @@ $(function() {
 
   });
 });
-
-
