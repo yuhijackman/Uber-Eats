0241755bb186685d7d2ed91731aa548a48708d29
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-11 17:21:35 +0900

    Delete unnecessary association

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :menus
-  has_many :orders
   validates :name, presence: true
   scope :including_name_or_genre, ->(word){ where("name like '%" + word + "%'").or(Restaurant.where("genre like '%" + word + "%'")) }
 end
