7a106603cb770ed819fe87ee5e25cf96ecf72658
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-11 17:18:48 +0900

    Increase readability

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,8 +1,10 @@
 class MenusController < ApplicationController
+
   def search
     @menu = Menu.find(params[:menu_id])
     respond_to do |format|
       format.json { render json: @menu }
     end
   end
+
 end
