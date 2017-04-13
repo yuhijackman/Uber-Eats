ca4e113f729c11a878e63b3baeef1b257d452044
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-13 19:25:02 +0900

    Add Set Modal Function in js

--- app/assets/javascripts/restaurant.js
+++ app/assets/javascripts/restaurant.js
@@ -1,4 +1,22 @@
 $(function() {
+
+  function setModal() {
+    $('.modal-menu__list__col').remove();
+    var modal = $( '<div class= "modal-menu__list__col col-md-4">'
+              + '<div class= "modal-menu__list__title">'
+              + '</div>'
+              + '<div class= "modal-menu__list__detail">'
+              + '</div>'
+              + '<div class="modal-footer">'
+              + '</div>'
+              + '<div class= "modal-sender">'
+              + '<input type="submit" name="commit" value="カートに追加" class="btn btn-success btn-lg btn-block" data-disable-with="カートに追加">'
+              + '</div>'
+              + '</div>');
+    $('.modal-menu__list__row').prepend(modal);
+  }
+
+
   function completeOrder(menu) {
     var name = menu.name;
     var detail = menu.detail;
@@ -23,6 +41,7 @@ $(function() {
     })
 
     .done(function(data) {
+      setModal();
       completeOrder(data);
     })
 
