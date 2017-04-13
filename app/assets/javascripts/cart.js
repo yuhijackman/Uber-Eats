3351e1e91e6c4a652b1ff6ca2b440459393ca357
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-13 19:25:21 +0900

    Modify cart js file

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -3,24 +3,26 @@ $(function() {
     var name = cart_detail.name;
     var price = cart_detail.price;
     var menu_id = cart_detail.id;
-    var item = $( '<div class= "cart__list__content__left__col col-md-2">'
-               + '</div>'
-               + '<div class= "cart__list__content__center__col col-md-7">'
-               + '<class = "modal-body">'
+    var item = $( '<div class= "wrapper">'
+               + '<div class= "cart__list__content__center__col col-md-7 col-md-offset-1">'
+               + '<div class = "modal-name">'
                + name
                + '</div>'
                + '</div>'
-               + '<div class= "cart__list__content__right__col col-md-3">'
+               + '<div class= "cart__list__content__right__col col-md-4">'
                + '<div class= "modal-price">'
                + "¥" + price
+               + '</div>'
                + '</div>'
                + '</div>');
     $('.cart__list__content').append(item);
   }
 
   function showAlert() {
-    var notification = $( '<div class= "empty__notice col-md-12">'
+    var notification = $( '<div class= "wrapper">'
+                       + '<div class= "empty__notice col-md-12">'
                        + "カートが空です。"
+                       + '</div>'
                        + '</div>');
     $('.cart__list__content').append(notification);
   }
@@ -33,6 +35,7 @@ $(function() {
     })
 
     .done(function(data) {
+       $('.wrapper').remove();
       if(data == null){
         $('.cart__list__footer').remove();
         showAlert();
