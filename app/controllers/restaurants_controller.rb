0f68f76cc89e59e922e010a693406d035833de85
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-14 18:19:42 +0900

    Set New Rules For Auto Favorite Function

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,13 @@ class RestaurantsController < ApplicationController
   def index
     session.delete(:cart)
     @restaurants = Restaurant.all
-    @favorites = Favorite.where(user_id: current_user.id)
+    favorites = Favorite.where(user_id: current_user.id, created_at: 1.week.ago..Time.current)
+    if favorites.present?
+      menu_ids = favorites.group(:menu_id).order('count_menu_id DESC').limit(3).count(:menu_id).keys
+      @favorites = menu_ids.map{ |id| Favorite.find_by(menu_id: id) }
+    else
+      @favorites = Favorite.where(user_id: current_user.id).last(3)
+    end
   end
 
   def show
