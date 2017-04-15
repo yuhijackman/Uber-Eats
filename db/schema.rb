115dfcc25a66c7d424b378e69d1eb5df03ce405a
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-15 14:27:04 +0900

    Install Gon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412062326) do
+ActiveRecord::Schema.define(version: 20170414111501) do
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "menu_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170412062326) do
     t.index ["menu_id"], name: "index_favorites_on_menu_id", using: :btree
     t.index ["restaurant_id"], name: "index_favorites_on_restaurant_id", using: :btree
     t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "menus", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -42,13 +49,14 @@ ActiveRecord::Schema.define(version: 20170412062326) do
   end
 
   create_table "restaurants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                     null: false
+    t.string   "name",                      null: false
     t.string   "email"
-    t.text     "address",    limit: 65535
+    t.text     "address",     limit: 65535
     t.string   "genre"
-    t.text     "image",      limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "image",       limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "likes_count"
   end
 
   create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
