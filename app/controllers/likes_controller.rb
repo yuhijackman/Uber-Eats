d3742b2c5851e39ccb2945e2678ab48556ef5a10
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-15 14:25:07 +0900

    Set Controller to Create and Destroy Like

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+
+  def create
+    @like = Like.create(user_id: current_user.id, restaurant_id: params[:restaurant_id])
+    @likes = Like.where(restaurant_id: params[:restaurant_id])
+    respond_to do |format|
+      format.json { render json: @likes }
+    end
+  end
+
+  def destroy
+    @like = current_user.likes.find_by(restaurant_id: params[:restaurant_id])
+    @like.destroy
+    @likes = Like.where(restaurant_id: params[:restaurant_id])
+    respond_to do |format|
+      format.json { render json: @likes }
+    end
+  end
+
+end
