42740e6a95e98ea4b1b7b35d101287030833de63
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-17 14:19:27 +0900

    Refactoring Restaurants Controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,19 +3,13 @@ class RestaurantsController < ApplicationController
   before_action :find_specific_restaurant, only: [:show, :location]
 
   def index
-
     session.delete(:cart)
     @restaurants = Restaurant.all
-    favorites = Favorite.where(user_id: current_user.id, created_at: 1.week.ago..Time.current)
-    if favorites.present?
-      menu_ids = favorites.group(:menu_id).order('count_menu_id DESC').limit(3).count(:menu_id).keys
-      @favorites = menu_ids.map{ |id| Favorite.find_by(menu_id: id) }
-    else
-      @favorites = Favorite.where(user_id: current_user.id).last(3)
-    end
+    favorites = Favorite.where(user_id: current_user.id, created_at: 1.week.ago..Time.current).present? || Favorite.where(user_id: current_user.id)
+    menu_ids = favorites.group(:menu_id).order('count_menu_id DESC').limit(3).count(:menu_id).keys
+    @favorites = menu_ids.map{ |id| Favorite.find_by(menu_id: id) }
     @recommends_info = []
     Restaurant.show_recommends(current_user, @recommends_info)
-
   end
 
   def show
