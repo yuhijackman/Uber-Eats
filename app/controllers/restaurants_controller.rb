5ce8ed18d1954050bfcef41a8f935ff069dee813
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-18 13:27:15 +0900

    TEST3

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,42 @@ class RestaurantsController < ApplicationController
   before_action :find_specific_restaurant, only: [:show, :location]
 
   def index
+    session.delete(:cart)
+    @restaurants = Restaurant.all
+    favorites = Favorite.where(user_id: current_user.id, created_at: 1.week.ago..Time.current).present? || Favorite.where(user_id: current_user.id)
+    menu_ids = favorites.group(:menu_id).order('count_menu_id DESC').limit(3).count(:menu_id).keys
+    @favorites = menu_ids.map{ |id| Favorite.find_by(menu_id: id) }
+    @recommends_info = []
+    recommends_ids = []
+    other_users_likes = []
+    c_restaurants = []
+    o_restaurants = []
+
+    current_user_likes = current_user.likes
+    current_user_likes.each do |c|
+      item = c.restaurant_id
+      c_restaurants << item
+    end
+
+    users = User.all
+    other_users = users.reject { |u| u.id == current_user.id }
+    other_users.each do |user|
+      other_user_likes = user.likes
+      other_user_likes.each do |o|
+        item = o.restaurant_id
+        o_restaurants <<  item
+      end
+    end
+
+    if c_restaurants & o_restaurants
+      recommend = o_restaurants - c_restaurants
+      recommends_ids += recommend
+    end
+
+    recommends_ids.each do |r|
+      restaurant = Restaurant.find(r)
+      @recommends_info << restaurant
+    end
   end
 
   def show
