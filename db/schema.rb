c2d0ad5713f2520e67dd5035d8a94f79a5fac6aa
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-19 14:19:33 +0900

    Add Column To Catering

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419005419) do
+ActiveRecord::Schema.define(version: 20170419043813) do
 
   create_table "caterings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "menu_id"
-    t.datetime "arrived_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "arrived_at",               null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "address",    limit: 65535
     t.index ["menu_id"], name: "index_caterings_on_menu_id", using: :btree
     t.index ["user_id"], name: "index_caterings_on_user_id", using: :btree
   end
