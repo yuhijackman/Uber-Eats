72973fa91dc293b0b77622e354d33c183d12b98b
yuhijackman <koizumitomohi@YuhinoMacBook-Pro.local>
2017-04-20 08:40:19 +0900

    Generate And Set Mailer.rb

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,19 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_confirm.subject
+  #
+  def sendmail_confirm(user, catering)
+    binding.pry
+    @item = catering.menu.name
+    @price = catering.menu.price
+    @time = catering.arrived_at
+    @address = catering.address
+    @message = "ケータリング注文が完了しました"
+    @name = user.name
+
+    mail to: user.email
+  end
+end
